40348f44ba80f230cee94aae13d3dc92b01a86c4
Kenny Henbest <lekenbest@gmail.com>
2019-02-06 17:14:24 -0700

    rps.unrefined

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+let userChoice = '' //this is a global variable
 
 
 let playerChoice = {
@@ -8,10 +8,15 @@ let playerChoice = {
     paper: 'paper',
     scissors: 'scissors'
 }
+let cpuChoice = {
+    rock: 'rock',
+    paper: 'paper',
+    scissors: 'scissors'
+}
 function drawButtons() {
     let template = ''
     for (let input in playerChoice) {
-        template += `<button class="${input}-btn" onclick="play('${input}')">${input}</button>`
+        template += `<button class="${input}-btn" onclick="setUserChoice('${input}')">${input}</button>`
     }
     document.getElementById('buttons').innerHTML = template
 }
@@ -20,17 +25,59 @@ drawButtons()
 
 
 
-function play(playerChoice) {
+function play(cpuChoice) {
+    userChoice
+    if (userChoice == 'rock') {
+        if (cpuChoice == 'rock') {
+            document.getElementById('result').innerHTML = 'DRAW';
+        }
+        else if (cpuChoice == 'paper') {
+            document.getElementById('result').innerHTML = 'LOSE';
+        }
+        else if (cpuChoice == 'scissors') {
+            document.getElementById('result').innerHTML = 'WIN'
+        }
+    }
+    if (userChoice == 'paper') {
+        if (cpuChoice == 'paper') {
+            document.getElementById('result').innerHTML = 'DRAW';
+        }
+        else if (cpuChoice == 'scissors') {
+            document.getElementById('result').innerHTML = 'LOSE';
+        }
+        else if (cpuChoice == 'rock') {
+            document.getElementById('result').innerHTML = 'WIN'
+        }
+    }
+    if (userChoice == 'scissors') {
+        if (cpuChoice == 'scissors') {
+            document.getElementById('result').innerHTML = 'DRAW';
+        }
+        else if (cpuChoice == 'rock') {
+            document.getElementById('result').innerHTML = 'LOSE';
+        }
+        else if (cpuChoice == 'paper') {
+            document.getElementById('result').innerHTML = 'WIN'
+        }
+    }
+}
 
-    if (playerChoice == 'rock') {
-        document.getElementById('result').innerHTML = 'DRAW'
-    }
-    else if (playerChoice == 'paper') {
-        document.getElementById('result').innerHTML = 'WIN'
-    }
-    else if (playerChoice == 'scissors')
-        document.getElementById('result').innerHTML = 'LOSE'
+function setUserChoice(name) {
+    userChoice = name
+    //set the html of an element on the page equal to BATTLE.. vs already = battle
+}
+
+// random() sets the computers choice
+function random() {
+    let choices = Object.keys(cpuChoice)
+    let choiceIndex = Math.floor(Math.random() * choices.length)
+    let choice = choices[choiceIndex]
+    return choice
+}
+
+
+function battle() {
+    let cpuChoice = random();
+    play(cpuChoice);
 
 }
-play()
-
